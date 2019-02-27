331f83369f3ef4a595f4a99f24e92d44f7bab753
Jennifer Proust <jennifer.proust@gmail.com>
2019-02-27 12:33:05 +0100

    test(E2E): Loggin HTTP Requests

--- e2e/index.js
+++ e2e/index.js
@@ -1,3 +1,4 @@
+import { RequestLogger } from 'testcafe';
 import HomePage from './models/home';
 import PostsPage from './models/posts';
 import ArticlePage from './models/article';
@@ -10,7 +11,14 @@ const articlePage = new ArticlePage();
 const addPage = new AddPage();
 const toastPage = new ToastPage();
 
-fixture`Navigation`.page`http://localhost:5000`.beforeEach(async t => {
+const url = 'http://localhost:5000';
+
+const logger = RequestLogger(
+  { url, method: 'get' },
+  { logResponseHeaders: true, logResponseBody: true }
+);
+
+fixture`Navigation`.page(url).beforeEach(async t => {
   await t.click(homePage.startBtn);
   await postsPage.isPageDisplayed();
 });
@@ -21,10 +29,16 @@ test('Access to an article from the home page', async t => {
   await t.expect(await articlePage.title.innerText).eql(text);
 });
 
-test('Access to the form and posting an article, coming from home', async t => {
-  await t.click(await postsPage.addBtn);
+test.requestHooks(logger)(
+  'Access to the form and posting an article, coming from home',
+  async t => {
+    await t.click(await postsPage.addBtn);
 
-  await addPage.isPageDisplayed();
-  await addPage.submitForm('How TestCafe is awesome!', 'Bla Bli Blou');
-  await toastPage.isToastDisplayed();
-});
+    await addPage.isPageDisplayed();
+    await addPage.submitForm('How TestCafe is awesome!', 'Bla Bli Blou');
+
+    await t.expect(logger.contains(r => r.response.statusCode === 200)).ok();
+
+    await toastPage.isToastDisplayed();
+  }
+);
